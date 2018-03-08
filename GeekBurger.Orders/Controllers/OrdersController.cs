3cc7b9ce37d675d884bf34dc60c41525b1d03344
Glaucia Oliveira <gap.oliveira@yahoo.com.br>
2018-03-08 16:28:15 -0300

    Add project files.

---
+++ GeekBurger.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekBurger.Orders.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Orders")]
+    public class OrdersController : Controller
+    {
+
+        private IOrdersRepository _ordersRepository;
+        private IMapper _mapper;
+
+        [HttpGet("{id}", Name = "GetOrdersByOrderId")]
+        public IActionResult GetOrdersByOrderId(Guid orderId)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateStatusOrder(Guid orderId, [FromBody] JsonPatchDocument<Order> order)
+        {
+            return Ok();
+        }
+
+        [HttpPost()]
+        public IActionResult GetAllOrders()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ GeekBurger.Orders/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using GeekBurger.Products.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Orders.Model
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime DeliverDate { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<ProductToGet> Products { get; set; } = new List<ProductToGet>();
+
+    }
+}
