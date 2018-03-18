ec37cfe9c07880e3bb2adf8fdd4302f9b300b068
Jacqueline Sabino <sabino.jacke@hotmail.com>
2018-03-18 20:05:05 -0300

    Inclusao de novos arquivos

---
+++ GeekBurger.Orders.Contract/OrderStatusPagamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Orders.Contract
+{
+   public class OrderStatusPagamento
+    {
+        public enum StatusPagamento
+        {
+            Pago,
+            Pendente
+        };
+    }
+}
