ac2696fa7d84fce5ccf07339b13f5e1b82ae19d1
Jacqueline Sabino <sabino.jacke@hotmail.com>
2018-03-22 21:54:21 -0300

    Classes de pagamento

---
+++ GeekBurger.Orders.Contract/OrderPaidToUpsert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Orders.Contract
+{
+   public class OrderPaidToUpsert
+    {
+     
+        public DateTime RequestDate { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string OrderStatus { get; set; }
+        public string OrderStatusPagamento { get; set; }
+        public string NumberCartaoCredito { get; set; }
+        public string NameCartaoCredito { get; set; }
+        public string Cvv { get; set; }
+        public string ValidadeDate { get; set; }
+    }
+}
