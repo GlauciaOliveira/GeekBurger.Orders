dd381e1f3272499e24f53e7521243a96c8530849
Jacqueline Sabino <sabino.jacke@hotmail.com>
2018-03-26 21:48:21 -0300

    Ajuste da classe Order

--- GeekBurger.Orders/Extension/OrderContextExtension.cs
+++ GeekBurger.Orders/Extension/OrderContextExtension.cs
@@ -15,12 +15,14 @@ namespace GeekBurger.Orders.Extension
             context.Order.RemoveRange(context.Order);
             context.SaveChanges();
             context.Order.AddRange(new List<Order> {
-                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatus="Pendente"},
-                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatus="Finalizado"},
-                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatus="Pendente"}
+                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatusPagamento="Pendente", NumberCartaoCredito="9998666444333",Cvv="176",NameCartaoCredito="Andre Luiz", ValidadeDate="02/2024"},
+                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatusPagamento="Finalizado",NumberCartaoCredito="99986688888",Cvv="189",NameCartaoCredito="Luiza Mel", ValidadeDate="08/2028"},
+                new Order { OrderId = Guid.NewGuid(), RequestDate = DateTime.Now, PaymentDate =  DateTime.Now, OrderStatusPagamento="Pendente",NumberCartaoCredito="9998666477777",Cvv="112",NameCartaoCredito="Rodrigo Santos", ValidadeDate="04/2028"}
             });
 
-            context.SaveChanges();
+
+
+        context.SaveChanges();
         }
     }
 }
--- GeekBurger.Orders/Model/Order.cs
+++ GeekBurger.Orders/Model/Order.cs
@@ -11,8 +11,11 @@ namespace GeekBurger.Orders.Model
         public Guid OrderId { get; set; }
         public DateTime RequestDate { get; set; }
         public DateTime PaymentDate { get; set; }
-        public string OrderStatus { get; set; }
         public string OrderStatusPagamento { get; set; }
+        public string NumberCartaoCredito { get; set; }
+        public string NameCartaoCredito { get; set; }
+        public string Cvv { get; set; }
+        public string ValidadeDate { get; set; }
 
     }
 }
